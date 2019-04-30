527004dec81904864d2f6d0f7de7398b97173fdc
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-30 18:52:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,11 @@ class Shoe
   def initialize brand
     @brand = brand
   end
-  attr_reader :brand
 
+  attr_reader :brand
+  attr_accessor :condition, :material, :size, :color
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
 end
