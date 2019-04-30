5b8e9e9c838e4554c0358906f28055d3731b49c8
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-30 18:40:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
-
     def initialize(title)
       @title = title
     end
+
   attr_reader :title
 
   attr_accessor :author, :page_count, :genre
@@ -10,5 +10,4 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
